3ebc1a5cfd35fd633c17b82b4ad8c6c9e967b7ce
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2020-06-15 23:43:06 -0500

    Funciona  carga foto

--- app/src/main/java/com/example/menusdenavegacion/ui/vender/ProductoVerderActivity.java
+++ app/src/main/java/com/example/menusdenavegacion/ui/vender/ProductoVerderActivity.java
@@ -1,7 +1,12 @@
 package com.example.menusdenavegacion.ui.vender;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +14,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.menusdenavegacion.R;
 
 public class ProductoVerderActivity extends AppCompatActivity {
-
+    ImageView imagen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verder_producto);
+
+        imagen = (ImageView) findViewById(R.id.imagemId);
+    }
+
+    public void onclick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Selecione la aplicación "),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
     }
 }
