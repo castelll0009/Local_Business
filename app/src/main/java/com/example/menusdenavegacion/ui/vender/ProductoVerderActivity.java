800f4e6f3ca8b39a49386716edabce6aa333e65c
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2020-06-15 22:57:47 -0500

    agregamos el metodo oncreate a la clase verder producto

--- app/src/main/java/com/example/menusdenavegacion/ui/vender/ProductoVerderActivity.java
+++ app/src/main/java/com/example/menusdenavegacion/ui/vender/ProductoVerderActivity.java
@@ -1,4 +1,18 @@
 package com.example.menusdenavegacion.ui.vender;
 
-public class ProductoVerderActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.menusdenavegacion.R;
+
+public class ProductoVerderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verder_producto);
+    }
 }
